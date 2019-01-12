75bb6e829bd8ecdeed241d36584ea31639782976
Donovon <donovonjenson@hotmail.com>
2019-01-12 11:41:20 -0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 //Don't forget to create a production build for this
 
 module.exports = {
-    entry: './Client/index.js',
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-      path: __dirname + '/Client/webpackOutput',
-      publicPath: '/',
-      filename: 'bundle.js'
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-      contentBase: './Client',
-    }
-  };
+	entry: "./Client/index.js",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			}
+		]
+	},
+	resolve: {
+		extensions: ["*", ".js", ".jsx"]
+	},
+	output: {
+		path: __dirname + "/Client/webpackOutput",
+		publicPath: "/",
+		filename: "bundle.js"
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		contentBase: "./Client",
+	}
+};
