91b7956553c2d739f7fb3c3a234d8d703ec0af97
Donovon <donovonjenson@hotmail.com>
2019-01-12 20:07:58 -0800

    Add courses page

--- Client/components/app.js
+++ Client/components/app.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./homePage/home.js";
+import Courses from "./courses/courses.js";
 
-import NavBar from "./navBar/navbar.js";
+//import NavBar from "./navBar/navbar.js";
 
 export default class App extends React.Component{
 	render(){
 		return (
-			<NavBar>
-				<Switch>
-					<Route
-						exact
-						path="/"
-						component={HomePage}
-					/>
-				</Switch>
-			</NavBar>
+			<Switch>
+				<Route
+					exact
+					path="/courses"
+					component={Courses}
+				/>
+				<Route
+					exact
+					path="/"
+					component={HomePage}
+				/>
+			</Switch>
 
 		);
 	}
